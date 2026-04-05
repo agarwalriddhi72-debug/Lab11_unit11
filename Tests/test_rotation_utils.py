a05fd7bcddbad903e5f8a9648f5e76ea9ca1edd0
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-05 17:59:35 -0400

    Testing positive inputs

--- Tests/test_rotation_utils.py
+++ Tests/test_rotation_utils.py
@@ -10,3 +10,15 @@ Date: 04/05/2026
 import pytest
 
 from rotation_utils import adjust_rotation
+
+
+#Test normal input
+def test_positive_input_100() -> None:
+    assert adjust_rotation(100) == 100
+
+#Test large inputs
+def test_positive_input_460() -> None:
+    assert adjust_rotation(460) == 100
+
+def test_positive_input_820() -> None:
+    assert adjust_rotation(820) == 100
