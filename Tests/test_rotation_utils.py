8a874aed4102546bacb72aead71d6cb277b6e4e5
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-05 17:49:53 -0400

    Test rotation file

---
+++ Tests/test_rotation_utils.py
@@ -0,0 +1,12 @@
+"""
+Program Name: test_rotation_utils
+Name: Riddhi Agarwal
+Purpose: The purpose of this program is to write test cases in order to 
+         verify that the adjust_rotation function works correctly for different inputs.
+         This program contains pytest.
+Date: 04/05/2026
+"""
+
+import pytest
+
+from rotation_utils import adjust_rotation
